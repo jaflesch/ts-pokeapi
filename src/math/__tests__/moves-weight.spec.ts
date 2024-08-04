92b2392ae6c16ff63f4ae593d2c8588ea6504cb6
Jean Flesch <jean.flesch93@gmail.com>
2024-08-04 16:13:36 -0300

    refactor: get move power by weight regarding move name

--- src/math/__tests__/moves-weight.spec.ts
+++ src/math/__tests__/moves-weight.spec.ts
@@ -1,10 +1,12 @@
 import { describe, expect, it } from '@jest/globals';
 import {
-  getMoveBasePowerByWeight,
-  getMoveBasePowerByRelativeWeight,
+  getLowKickBasePower,
+  getGrassKnotBasePower,
+  getHeatCrashBasePower,
+  getHeavySlamBasePower,
 } from '../moves-weight';
 
-describe('Heavy Slam / Heat Crash damage power', () => {
+describe('Heavy Slam base power', () => {
   const weights = {
     emboar: 150,
     snorlax: 460,
@@ -16,7 +18,7 @@ describe('Heavy Slam / Heat Crash damage power', () => {
 
   it('returns 40 BP move', () => {
     expect(
-      getMoveBasePowerByRelativeWeight({
+      getHeavySlamBasePower({
         user: weights.snorlax,
         target: weights.hariyama,
       }),
@@ -25,7 +27,7 @@ describe('Heavy Slam / Heat Crash damage power', () => {
 
   it('returns 60 BP move', () => {
     expect(
-      getMoveBasePowerByRelativeWeight({
+      getHeavySlamBasePower({
         user: weights.snorlax,
         target: weights.arcanine,
       }),
@@ -34,7 +36,7 @@ describe('Heavy Slam / Heat Crash damage power', () => {
 
   it('returns 80 BP move', () => {
     expect(
-      getMoveBasePowerByRelativeWeight({
+      getHeavySlamBasePower({
         user: weights.snorlax,
         target: weights.emboar,
       }),
@@ -43,7 +45,7 @@ describe('Heavy Slam / Heat Crash damage power', () => {
 
   it('returns 100 BP move', () => {
     expect(
-      getMoveBasePowerByRelativeWeight({
+      getHeavySlamBasePower({
         user: weights.snorlax,
         target: weights.graveler,
       }),
@@ -52,7 +54,7 @@ describe('Heavy Slam / Heat Crash damage power', () => {
 
   it('returns 120 BP move', () => {
     expect(
-      getMoveBasePowerByRelativeWeight({
+      getHeavySlamBasePower({
         user: weights.snorlax,
         target: weights.jumpluff,
       }),
@@ -60,7 +62,63 @@ describe('Heavy Slam / Heat Crash damage power', () => {
   });
 });
 
-describe('Grass Knot / Low kick damage power', () => {
+describe('Heat Crash base power', () => {
+  const weights = {
+    emboar: 150,
+    snorlax: 460,
+    jumpluff: 3,
+    arcanine: 155,
+    graveler: 105,
+    hariyama: 253.8,
+  };
+
+  it('returns 40 BP move', () => {
+    expect(
+      getHeatCrashBasePower({
+        user: weights.snorlax,
+        target: weights.hariyama,
+      }),
+    ).toBe(40);
+  });
+
+  it('returns 60 BP move', () => {
+    expect(
+      getHeatCrashBasePower({
+        user: weights.snorlax,
+        target: weights.arcanine,
+      }),
+    ).toBe(60);
+  });
+
+  it('returns 80 BP move', () => {
+    expect(
+      getHeatCrashBasePower({
+        user: weights.snorlax,
+        target: weights.emboar,
+      }),
+    ).toBe(80);
+  });
+
+  it('returns 100 BP move', () => {
+    expect(
+      getHeatCrashBasePower({
+        user: weights.snorlax,
+        target: weights.graveler,
+      }),
+    ).toBe(100);
+  });
+
+  it('returns 120 BP move', () => {
+    expect(
+      getHeatCrashBasePower({
+        user: weights.snorlax,
+        target: weights.jumpluff,
+      }),
+    ).toBe(120);
+  });
+});
+
+describe('Grass Knot base power', () => {
   const weights = {
     gastly: 0.1,
     vulpix: 9.9,
@@ -79,35 +137,73 @@ describe('Grass Knot / Low kick damage power', () => {
     scolipede: 200.5,
   };
   it('returns 20 BP move', () => {
-    expect(getMoveBasePowerByWeight(weights.gastly)).toBe(20);
-    expect(getMoveBasePowerByWeight(weights.vulpix)).toBe(20);
+    expect(getGrassKnotBasePower(weights.gastly)).toBe(20);
   });
 
   it('returns 40 BP move', () => {
-    expect(getMoveBasePowerByWeight(weights.kakuna)).toBe(40);
-    expect(getMoveBasePowerByWeight(weights.purrlolin)).toBe(40);
-    expect(getMoveBasePowerByWeight(weights.nidorina)).toBe(40);
-    expect(getMoveBasePowerByWeight(weights.staraptor)).toBe(40);
+    expect(getGrassKnotBasePower(weights.kakuna)).toBe(40);
+    expect(getGrassKnotBasePower(weights.purrlolin)).toBe(40);
   });
 
   it('returns 60 BP move', () => {
-    expect(getMoveBasePowerByWeight(weights.seadra)).toBe(60);
-    expect(getMoveBasePowerByWeight(weights.hitmonlee)).toBe(60);
+    expect(getGrassKnotBasePower(weights.seadra)).toBe(60);
   });
 
   it('returns 80 BP move', () => {
-    expect(getMoveBasePowerByWeight(weights.zweilous)).toBe(80);
-    expect(getMoveBasePowerByWeight(weights.hitmonchan)).toBe(80);
-    expect(getMoveBasePowerByWeight(weights.bergmite)).toBe(80);
+    expect(getGrassKnotBasePower(weights.zweilous)).toBe(80);
+    expect(getGrassKnotBasePower(weights.hitmonchan)).toBe(80);
   });
 
   it('returns 100 BP move', () => {
-    expect(getMoveBasePowerByWeight(weights.venusaur)).toBe(100);
-    expect(getMoveBasePowerByWeight(weights.rampardos)).toBe(100);
+    expect(getGrassKnotBasePower(weights.venusaur)).toBe(100);
   });
 
   it('returns 120 BP move', () => {
-    expect(getMoveBasePowerByWeight(weights.crustle)).toBe(120);
-    expect(getMoveBasePowerByWeight(weights.scolipede)).toBe(120);
+    expect(getGrassKnotBasePower(weights.crustle)).toBe(120);
+  });
+});
+
+describe('Grass Knot base power', () => {
+  const weights = {
+    gastly: 0.1,
+    vulpix: 9.9,
+    kakuna: 10,
+    purrlolin: 10.1,
+    nidorina: 20,
+    staraptor: 24.9,
+    seadra: 25,
+    hitmonlee: 49.8,
+    zweilous: 50,
+    hitmonchan: 50.2,
+    bergmite: 99.5,
+    venusaur: 100,
+    rampardos: 102.5,
+    crustle: 200,
+    scolipede: 200.5,
+  };
+  it('returns 20 BP move', () => {
+    expect(getLowKickBasePower(weights.vulpix)).toBe(20);
+  });
+
+  it('returns 40 BP move', () => {
+    expect(getLowKickBasePower(weights.nidorina)).toBe(40);
+    expect(getLowKickBasePower(weights.staraptor)).toBe(40);
+  });
+
+  it('returns 60 BP move', () => {
+    expect(getLowKickBasePower(weights.hitmonlee)).toBe(60);
+  });
+
+  it('returns 80 BP move', () => {
+    expect(getLowKickBasePower(weights.hitmonchan)).toBe(80);
+    expect(getLowKickBasePower(weights.bergmite)).toBe(80);
+  });
+
+  it('returns 100 BP move', () => {
+    expect(getLowKickBasePower(weights.rampardos)).toBe(100);
+  });
+
+  it('returns 120 BP move', () => {
+    expect(getLowKickBasePower(weights.scolipede)).toBe(120);
   });
 });
