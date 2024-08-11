20a12394d3d977ed766eee9ed87faa5e6e3dab1a
Jean Flesch <jean.flesch93@gmail.com>
2024-08-11 16:21:29 -0300

    doc: update doc's homepage

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -1,51 +1,14 @@
+import React from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import { FeatureItem, featureList } from './utils';
 
-type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: JSX.Element;
-};
-
-const FeatureList: FeatureItem[] = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
-
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, source, description }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <img src={source} className={styles.featureImage} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
@@ -55,14 +18,24 @@ function Feature({title, Svg, description}: FeatureItem) {
   );
 }
 
-export default function HomepageFeatures(): JSX.Element {
+function ColumnRowBreak({ index }: { index: number }) {
+  return index !== 0 && index % 3 === 0 ? (
+    <div className={`col col--12 ${styles.rowBreak}`} />
+  ) : null;
+}
+
+export default function HomepageFeatures(): React.JSX.Element {
   return (
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
+          {featureList.map((props, idx) => (
+            <>
+              <ColumnRowBreak index={idx} />
+              <Feature key={idx} {...props} />
+            </>
           ))}
+          {}
         </div>
       </div>
     </section>
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,40 +1,60 @@
+import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
-
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+        <div className={`row ${styles.wrapper}`}>
+          <div>
+            <img src="img/logo-negative.png" className={styles.logo} />
+          </div>
+          <div className={styles.rowGap}>
+            <Heading as="h1" className="hero__title">
+              {siteConfig.title}
+            </Heading>
+            <p className="hero__subtitle">
+              Zero dependency, Lightweight, Extensible.
+              <br />A Multipurpose TypeScript library
+            </p>
+          </div>
         </div>
       </div>
     </header>
   );
 }
 
-export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+export default function Home(): React.JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
+      <div className={styles.buttonsWrapper}>
+        <div className={styles.buttons}>
+          <Link
+            to="/docs/guides/getting-started"
+            className={`button button--secondary button--lg ${styles.buttonCustom}`}
+          >
+            Get started
+          </Link>
+          <Link
+            to="https://github.com/jaflesch/ts-pokeapi"
+            className={`button button--secondary button--lg ${styles.buttonCustom}`}
+          >
+            View on GitHub
+          </Link>
+        </div>
+      </div>
       <main>
         <HomepageFeatures />
       </main>
